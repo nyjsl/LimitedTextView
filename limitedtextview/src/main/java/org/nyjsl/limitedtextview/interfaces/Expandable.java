66c09d4a455b2a10bfc1ff545d7aa1b4013229e1
wx <weixings06@126.com>
2016-12-12 15:20:41 +0800

    Interface Modify

--- limitedtextview/src/main/java/org/nyjsl/limitedtextview/interfaces/Expandable.java
+++ limitedtextview/src/main/java/org/nyjsl/limitedtextview/interfaces/Expandable.java
@@ -2,12 +2,7 @@ package org.nyjsl.limitedtextview.interfaces;
 
 import android.view.View;
 
-public interface Expandable {
-
-    int STATE_SHRINK = 0;
-    int STATE_EXPAND = 1;
-
+public interface Expandable extends StateInterface {
     void onExpand(View view);
     void onShrink(View view);
-
 }
